11d8a68d728a8dc99efb53a2c2bc5a3c4e0f3e36
Misials <michalski.mikolaj10@wp.pl>
2022-09-28 18:42:51 +0200

    Chapter 11 - useEffect Hook, Chapter 12 - JSON Server, Chapter 13 - Fetch API Data. Finished

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import SearchItem from './SearchItem';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-	const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+	const API_URL = 'http://localhost:3500/items';
 
+	const [items, setItems] = useState([]);
 	const [newItem, setNewItem] = useState('');
 	const [search, setSearch] = useState('');
+	const [fetchError, setFetchError] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
 
-	const setAndSaveItems = newitems => {
-		setItems(newitems);
-		localStorage.setItem('shoppinglist', JSON.stringify(newitems));
-	};
+	useEffect(() => {
+		const fetchItems = async () => {
+			try {
+				const response = await fetch(API_URL);
+				if (!response.ok) throw Error('Did not receive expected data');
+				const listItems = await response.json();
+				setItems(listItems);
+				setFetchError(null);
+			} catch (err) {
+				setFetchError(err.message);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		setTimeout(fetchItems, 2000);
+	}, []);
 
 	const addItem = item => {
 		const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -24,17 +40,17 @@ function App() {
 			item,
 		};
 		const listItems = [...items, myNewItem];
-		setAndSaveItems(listItems);
+		setItems(listItems);
 	};
 
 	const handleCheck = id => {
 		const listItems = items.map(item => (item.id === id ? { ...item, checked: !item.checked } : item));
-		setAndSaveItems(listItems);
+		setItems(listItems);
 	};
 
 	const handleDelete = id => {
 		const listItems = items.filter(item => item.id !== id);
-		setAndSaveItems(listItems);
+		setItems(listItems);
 	};
 
 	const handleSubmit = e => {
@@ -49,11 +65,17 @@ function App() {
 			<Header title='Grocery List' />
 			<AddItem newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit} />
 			<SearchItem search={search} setSearch={setSearch} />
-			<Content
-				items={items.filter(item => item.item.toLowerCase().includes(search.toLowerCase()))}
-				handleCheck={handleCheck}
-				handleDelete={handleDelete}
-			/>
+			<main>
+				{isLoading && <p>Loading Items...</p>}
+				{fetchError && <p style={{ color: 'red' }}>{`Error: ${fetchError}`}</p>}
+				{!fetchError && !isLoading && (
+					<Content
+						items={items.filter(item => item.item.toLowerCase().includes(search.toLowerCase()))}
+						handleCheck={handleCheck}
+						handleDelete={handleDelete}
+					/>
+				)}
+			</main>
 			<Footer items={items} />
 		</div>
 	);
